65d1c81a9ede3316e7db3a59f20da2faa8e2bb42
Ryan <ryanjones@Ryans-MacBook-Pro.local>
2018-03-13 10:26:11 +0100

    set-up firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform, NavController, MenuController } from 'ionic-angular';
 import { TabsPage } from '../pages/tabs/tabs';
 import { SigninPage } from '../pages/signin/signin';
 import { SignupPage } from '../pages/signup/signup';
+import firebase from 'firebase';
 
 @Component({
   templateUrl: 'app.html'
@@ -13,6 +14,10 @@ export class MyApp {
   private signinPage: any = SigninPage;
   private signupPage: any = SignupPage;
   constructor(platform: Platform, private menuCtrl: MenuController) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyD0x_sxOXiD957_EUJIG0qqfS9-sUAY4LQ",
+      authDomain: "ionic-shopping-list-a7099.firebaseapp.com",
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
